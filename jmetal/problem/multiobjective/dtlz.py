e9047c9b1d8eced67f10d0b7505cc35d7019276f
ajnebro <antonio@lcc.uma.es>
2018-05-20 12:10:45 +0200

    Add problem DTLZ3

--- jmetal/problem/multiobjective/dtlz.py
+++ jmetal/problem/multiobjective/dtlz.py
@@ -94,3 +94,46 @@ class DTLZ2(FloatProblem):
 
     def get_name(self):
         return "DTLZ2"
+
+
+class DTLZ3(FloatProblem):
+    """ Problem DTLZ3
+    .. note:: Unconstrained problem. The default number of variables and objectives are, respectively, 12 and 3.
+    .. note:: Continuous problem having a convex Pareto front
+    """
+
+    def __init__(self, number_of_variables: int = 12, number_of_objectives = 3):
+        """Constructor
+        :param number_of_variables: number of decision variables of the problem
+        """
+        self.number_of_variables = number_of_variables
+        self.number_of_objectives = number_of_objectives
+        self.number_of_constraints = 0
+
+        self.lower_bound = self.number_of_variables * [0.0]
+        self.upper_bound = self.number_of_variables * [1.0]
+
+        FloatSolution.lower_bound = self.lower_bound
+        FloatSolution.upper_bound = self.upper_bound
+
+    def evaluate(self, solution: FloatSolution):
+        g = 0.0
+        k = self.number_of_variables - self.number_of_objectives + 1
+
+        for i in range (self.number_of_variables - k, self.number_of_variables):
+            g += (solution.variables[i] - 0.5) * (solution.variables[i] - 0.5) - \
+                 cos(20.0 * pi * (solution.variables[i] - 0.5))
+
+        g = 100.0 * (k + g)
+        for i in range(self.number_of_objectives):
+            solution.objectives[i] = 1.0 + g
+
+        for i in range(self.number_of_objectives):
+            for j in range(self.number_of_objectives - (i + 1)):
+                solution.objectives[i] *= cos(solution.variables[j]) * 0.5 * pi
+            if i != 0:
+                aux = self.number_of_objectives - (i + 1)
+                solution.objectives[i] *= sin(solution.variables[aux] * 0.5 * pi)
+
+    def get_name(self):
+        return "DTLZ3"
