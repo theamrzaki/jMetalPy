bbd5b3dde029e2ee520d33569e80c4ac92316170
ajnebro <antonio@lcc.uma.es>
2018-05-20 12:45:37 +0200

    Add problem DTLZ7

--- jmetal/problem/multiobjective/dtlz.py
+++ jmetal/problem/multiobjective/dtlz.py
@@ -250,9 +250,9 @@ class DTLZ6(FloatProblem):
         FloatSolution.upper_bound = self.upper_bound
 
     def evaluate(self, solution: FloatSolution):
-        g = 0.0
         k = self.number_of_variables - self.number_of_objectives + 1
 
+        g = 0.0
         for i in range (self.number_of_variables - k, self.number_of_variables):
             g += pow(solution.variables[i], 0.1)
 
@@ -275,3 +275,36 @@ class DTLZ6(FloatProblem):
 
     def get_name(self):
         return "DTLZ6"
+
+
+class DTLZ7(FloatProblem):
+    """ Problem DTLZ7
+    .. note:: Unconstrained problem. The default number of variables and objectives are, respectively, 22 and 3.
+    .. note:: Continuous problem having a discontinuous Pareto front
+    """
+
+    def __init__(self, number_of_variables: int = 22, number_of_objectives = 3):
+        """Constructor
+        :param number_of_variables: number of decision variables of the problem
+        """
+        self.number_of_variables = number_of_variables
+        self.number_of_objectives = number_of_objectives
+        self.number_of_constraints = 0
+
+        self.lower_bound = self.number_of_variables * [0.0]
+        self.upper_bound = self.number_of_variables * [1.0]
+
+        FloatSolution.lower_bound = self.lower_bound
+        FloatSolution.upper_bound = self.upper_bound
+
+    def evaluate(self, solution: FloatSolution):
+        k = self.number_of_variables - self.number_of_objectives + 1
+        g = 1.0 + (9.0 * sum(solution.variables[self.number_of_variables - k:])) / k
+        h = self.number_of_objectives - sum(
+            [x / (1.0 + g) * (1.0 + sin(3.0 * pi * x)) for x in solution.variables[:self.number_of_objectives - 1]])
+
+        solution.objectives[:self.number_of_objectives - 1] = solution.variables[:self.number_of_objectives - 1]
+        solution.objectives[-1] = (1.0 + g) * h
+
+    def get_name(self):
+        return "DTLZ7"
